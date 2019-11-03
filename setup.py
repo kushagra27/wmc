0ce28eac06ece5156b69946a10596c0a89e52b1a
kushagra27 <agarwalkush27@gmail.com>
2019-11-04 02:08:40 +0530

    v0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'wmc',         # How you named your package folder (MyLib)
-  packages = ['wmc'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  name = 'wmcvit',         # How you named your package folder (MyLib)
+  packages = ['wmcvit'],   # Chose the same as "name"
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'help it is',   # Give a short description about your library
   author = 'qwertybob',                   # Type in your name
   author_email = 'agarwalkuksh27@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/kushagra27/wmc',   # Provide either the link to your github or to your website
+  url = 'https://github.com/kushagra27/wmcvit',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
--- wmcvit/a.py
+++ wmcvit/a.py
@@ -1,5 +1,5 @@
 class only:
-	a = "your code goes here"
+ a = "your code goes here 2 "
 if __name__ == "__main__":
-        p1 = only()
-	print(p1.a)
+ p1 = only()
+ print(p1.a)
