98c348aeca4cd3b2f2f3ddfdb111a2137d598117
kushagra27 <30297338+kushagra27@users.noreply.github.com>
2019-11-04 02:29:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'qwertybob',                   # Type in your name
   author_email = 'agarwalkuksh27@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/kushagra27/wmcvit',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kushagra27/wmcvit/archive/0.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kushagra27/wmcvit/archive/0.4.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           
